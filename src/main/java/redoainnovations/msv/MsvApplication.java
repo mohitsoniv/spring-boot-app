5ab2060a5b84e11e140468747f2617917918f721
root <root@ip-172-31-71-80.ec2.internal>
2024-07-02 07:26:54 +0000

    commit for boot-app

---
+++ src/main/java/redoainnovations/msv/MsvApplication.java
@@ -0,0 +1,13 @@
+package redoainnovations.msv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/redoainnovations/msv/MsvApplicationTests.java
@@ -0,0 +1,13 @@
+package redoainnovations.msv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
